31514e7da154fb71a9182b906644c325ac9bf1f8
TKZues <137180128+TKZues@users.noreply.github.com>
2024-04-30 15:03:14 +0700

    tạo hàm init

---
+++ lib/helper/dependencies.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class X extends StatelessWidget {
+  const X({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
